44f3f98f57d97d3c2184b9b048acaab9555e6cdc
Konrad <konrad.hartwig@online.de>
2020-04-21 01:37:44 -0700

    Add simple user controller

--- src/handler/userHandler.ts
+++ src/handler/userHandler.ts
@@ -6,15 +6,41 @@ export default class UserHandler extends Handler {
 
     constructor() {
         super();
-        this.router.get("/", this.getAll.bind(this));
+        this.router.get("/", this.getAllUsers.bind(this));
+        this.router.get("/user", this.getUser.bind(this));
+        this.router.post("/user", this.createUser.bind(this));
     }
 
-    private async getAll(req: express.Request, res: express.Response): Promise<void> {
-        try {
-            const users = await User.getAll();
-            res.json(users);
-        } catch (e) {
-            console.log(e)
-        }
+    private async getAllUsers(req: express.Request, res: express.Response): Promise<void> {
+        const users = User.find((err: any, user: any) => {
+            if (err) {
+                res.send("Error!");
+            } else {
+                res.send(user);
+            }
+        });
+    }
+
+    private async getUser(req: express.Request, res: express.Response): Promise<void> {
+        const user = User.findById(req.params.id, (err: any, user: any) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(user);
+            }
+        });
+    }
+
+    private async createUser(req: express.Request, res: express.Response): Promise<void> {
+        console.log(req.body);
+        const user = new User(req.body);
+
+        user.save((err: any) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(user);
+            }
+        });
     }
 }
--- src/types/user.ts
+++ src/types/user.ts
@@ -3,15 +3,11 @@ import * as mongoose from 'mongoose';
 interface IUser extends mongoose.Document {
     email: string;
     password: string;
-    firstName: string;
-    lastName: string;
 }
 
 const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, index: true },
     password: {type: String, required: true, select: false},
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true}
 }, {
     versionKey: false
 })
